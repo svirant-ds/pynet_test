da0131f2427a8cbf14e8e508eb1a4f77bdf04d00
Stephen Virant <stephen.virant@docusign.com>
2019-06-20 15:27:38 -0700

    lists

---
+++ lists1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+list = ['this','is','almost','five','strings']
+list.append('one')
+list.append('more')
+list[1]
+list.pop(0)
+lg = len(list)
+print(f"the list is {lg} long\n")
+list.sort()
+print(list)
