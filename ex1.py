4d663e4789f98f05515fa67beef20c51befb91c4
Stephen Virant <stephen.virant@docusign.com>
2019-06-20 13:59:43 -0700

    ex2

---
+++ ex1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+my_var1 = "howdy doodie"
+my_var2 = "wolf manzer"
+my_var3 = "turd furgeson"
+my_var4 = input("Enter a 4th name: ")
+print(f"{my_var1:>30}, {my_var2:>30}, {my_var3:>30}, {my_var4:>30}")
+
