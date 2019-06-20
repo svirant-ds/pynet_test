afd397510a68e457d11f47478cea3bdd27fa93c7
Stephen Virant <stephen.virant@docusign.com>
2019-06-20 15:17:25 -0700

    maths 1.1

--- numbers.py
+++ numbers.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 num1 = int(input("enter a number: "))
 num2 = int(input("enter another number: "))
+rem1 = float(num1 % num2)
 print(f"sum: {num1 + num2}")
 print(f"diff: {num1 - num2}")
 print(f"product: {num1 * num2}")
-print(f"remainder: {num1 % num2}")
+print(f"remainder: {rem1}")
